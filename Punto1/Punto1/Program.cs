83c96d77f3aee4fc68a261f04632b81844a11512
Luisaferpc <luisacardona2206@gmail.com>
2024-10-07 21:02:46 -0500

    aignand variables

---
+++ Punto1/Punto1/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int numero, jugadores;
+        int N = 0;
+        Random aleatorio = new Random();
+
+        Console.WriteLine("CANTIDAD DE JUGADORES: ");
+        jugadores = Convert.ToInt32(Console.ReadLine());
+
+        if (jugadores > 0 || jugadores <= 2) {
+            Console.WriteLine("VAMOS A JUGAR! \n ADIVINA EL NÚMERO");
+            numero = Convert.ToInt32(Console.ReadLine());
+            N = aleatorio.Next(1, 50);
+        }
+       
+
+        
+
+  
+
+
+  
+
+
+    }
+}

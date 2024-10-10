b48fe7de5473d13637d4329c807a17e399ff881b
Luisaferpc <luisacardona2206@gmail.com>
2024-10-10 05:22:58 +0000

    verificacion debug

--- solpunto2/Program.cs
+++ solpunto2/Program.cs
@@ -19,66 +19,66 @@
 
         Console.WriteLine("NOMBRE DEL ESTUDIANTE: ");
         nombreEstudiante=Console.ReadLine();
-
-        Console.WriteLine("Ingresa la primera nota: ");
-        nota1= double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Ingresa la segunda nota: ");
-        nota2= double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Ingresa la tercera nota: ");
-        nota3= double.Parse(Console.ReadLine());
-
-        suma=nota1+nota2+nota3;
-        promedio=suma/3;
-
-        switch (promedio)
-        { 
-            case double notaFinal when (notaFinal>=4.5):
-
-            Console.WriteLine($"El estudiante {nombreEstudiante} está en la categoria EXCELENTE, {promedio} ");
-            break;
-
-            case double notaFinal when (notaFinal>= 4.0 && notaFinal<4.5):
-
-            Console.WriteLine($"El estudiante {nombreEstudiante} está en la categoria SOBRESALIENTE, {promedio} ");
-            break;
-
-            case double notaFinal when (notaFinal>= 3.5 && notaFinal<4.0):
-
-            Console.WriteLine($"El estudiante {nombreEstudiante} está en la categoria BUENO, {promedio} ");
-            break;
-
-             case double notaFinal when (notaFinal<3.5):
-
-            Console.WriteLine($"El estudiante {nombreEstudiante} está en la categoria BUENO, {promedio} ");
-            break;
-
-            
-            
-        }
-            if (promedio>=3.5)
-            {
-                ganadores++;
-            }
-            else
-            {
-                perdedores++;
-            }
         
-        Console.WriteLine("DESEA INGRESAR MAS ESTUDIANTES? (S/N) ");
-        string respuesta=Console.ReadLine().ToLower();
+                Console.WriteLine("Ingresa la primera nota: ");
+                nota1= double.Parse(Console.ReadLine());
 
-        if (respuesta=="n")
-        {
-            continuar=false;
-            Console.WriteLine($"EN TOTAL APROBARON {ganadores} ESTUDIANTES ");
-             Console.WriteLine($"EN TOTAL REPROBARON {perdedores} ESTUDIANTES ");
-        }
+                Console.WriteLine("Ingresa la segunda nota: ");
+                nota2= double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingresa la tercera nota: ");
+                nota3= double.Parse(Console.ReadLine());
+
+                suma=nota1+nota2+nota3;
+                promedio=suma/3;
+
+                switch (promedio)
+                { 
+                    case double notaFinal when (notaFinal>=4.5):
+
+                    Console.WriteLine($"El estudiante {nombreEstudiante} está en la categoria EXCELENTE, {promedio} ");
+                    break;
+
+                    case double notaFinal when (notaFinal>= 4.0 && notaFinal<4.5):
+
+                    Console.WriteLine($"El estudiante {nombreEstudiante} está en la categoria SOBRESALIENTE, {promedio} ");
+                    break;
+
+                    case double notaFinal when (notaFinal>= 3.5 && notaFinal<4.0):
+
+                    Console.WriteLine($"El estudiante {nombreEstudiante} está en la categoria BUENO, {promedio} ");
+                    break;
+
+                    case double notaFinal when (notaFinal<3.5):
+
+                    Console.WriteLine($"El estudiante {nombreEstudiante} está en la categoria BUENO, {promedio} ");
+                    break;
+
+                    
+                    
+                }
+                    if (promedio>=3.5)
+                    {
+                        ganadores++;
+                    }
+                    else
+                    {
+                        perdedores++;
+                    }
+                
+                Console.WriteLine("DESEA INGRESAR MAS ESTUDIANTES? (S/N) ");
+                string respuesta=Console.ReadLine().ToLower();
+
+                if (respuesta=="n")
+                {
+                    continuar=false;
+                    Console.WriteLine($"EN TOTAL APROBARON {ganadores} ESTUDIANTES ");
+                    Console.WriteLine($"EN TOTAL REPROBARON {perdedores} ESTUDIANTES ");
+                }
 
 
-        } while (continuar);
-
+                } while (continuar);
+         
     
     
     
